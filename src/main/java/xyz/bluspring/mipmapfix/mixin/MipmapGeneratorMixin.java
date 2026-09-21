4c6e421ca6c3f1ec3a2e447f9c5f2f8f384deed2
BluSpring <bluspringindohq@gmail.com>
2026-09-22 02:21:25 +0800

    make the mipmap fixes more mod compatible

--- src/main/java/xyz/bluspring/mipmapfix/mixin/MipmapGeneratorMixin.java
+++ src/main/java/xyz/bluspring/mipmapfix/mixin/MipmapGeneratorMixin.java
@@ -1,61 +1,46 @@
 package xyz.bluspring.mipmapfix.mixin;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import com.mojang.blaze3d.platform.NativeImage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
 import net.minecraft.client.renderer.texture.MipmapGenerator;
 import net.minecraft.util.Mth;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = MipmapGenerator.class, priority = 990)
 public abstract class MipmapGeneratorMixin {
-    @Shadow protected static boolean hasTransparentPixel(NativeImage image) {
-        throw new IllegalStateException();
-    };
+    @Inject(method = "generateMipLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/MipmapGenerator;hasTransparentPixel(Lcom/mojang/blaze3d/platform/NativeImage;)Z", shift = At.Shift.AFTER))
+    private static void kilt$getMaxMipLevel(NativeImage[] images, int mipLevel, CallbackInfoReturnable<NativeImage[]> cir, @Local(ordinal = 1) NativeImage[] nativeImages, @Share("maxMipLevel") LocalIntRef maxMipLevel) {
+        maxMipLevel.set(Math.min(
+            Mth.log2(Math.max(1, nativeImages[0].getWidth())),
+            Mth.log2(Math.max(1, nativeImages[0].getHeight()))
+        ));
+    }
 
-    @Shadow protected static int alphaBlend(int col0, int col1, int col2, int col3, boolean transparent) {
-        throw new IllegalStateException();
-    };
+    @ModifyArgs(method = "generateMipLevels", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/NativeImage;<init>(IIZ)V"))
+    private static void kilt$guardFromInvalidTexSize(Args args) {
+        args.set(0, Math.max(1, args.get(0)));
+        args.set(1, Math.max(1, args.get(1)));
+    }
 
-    /**
-     * @author BluSpring
-     * @reason no other way to add a continue, other than writing a fuckin' mixin plugin for it.
-     */
-    @Overwrite
-    public static NativeImage[] generateMipLevels(NativeImage[] images, int mipLevel) {
-        if (mipLevel + 1 <= images.length) {
-            return images;
-        } else {
-            NativeImage[] nativeImages = new NativeImage[mipLevel + 1];
-            nativeImages[0] = images[0];
-            boolean bl = hasTransparentPixel(nativeImages[0]);
+    @Definition(id = "l", local = @Local(type = int.class, ordinal = 4))
+    @Definition(id = "j", local = @Local(type = int.class, ordinal = 2))
+    @Expression("l < j")
+    @ModifyExpressionValue(method = "generateMipLevels", at = @At("MIXINEXTRAS:EXPRESSION"), require = 0, expect = 0)
+    private static boolean kilt$avoidBlendingPixelsIfInvalidMip(boolean original, @Local(ordinal = 1) int currentMipLevel, @Share("maxMipLevel") LocalIntRef maxMipLevel) {
+        if (currentMipLevel > maxMipLevel.get())
+            return false;
 
-            int maxMipLevel = Math.min(
-                Mth.log2(Math.max(1, nativeImages[0].getWidth())),
-                Mth.log2(Math.max(1, nativeImages[0].getHeight()))
-            );
-            for(int i = 1; i <= mipLevel; ++i) {
-                if (i < images.length) {
-                    nativeImages[i] = images[i];
-                } else {
-                    NativeImage nativeImage = nativeImages[i - 1];
-                    NativeImage nativeImage2 = new NativeImage(Math.max(1, nativeImage.getWidth() >> 1), Math.max(1, nativeImage.getHeight() >> 1), false);;
-                    if (i <= maxMipLevel) {
-                        int j = nativeImage2.getWidth();
-                        int k = nativeImage2.getHeight();
-
-                        for (int l = 0; l < j; ++l) {
-                            for (int m = 0; m < k; ++m) {
-                                nativeImage2.setPixelRGBA(l, m, alphaBlend(nativeImage.getPixelRGBA(l * 2 + 0, m * 2 + 0), nativeImage.getPixelRGBA(l * 2 + 1, m * 2 + 0), nativeImage.getPixelRGBA(l * 2 + 0, m * 2 + 1), nativeImage.getPixelRGBA(l * 2 + 1, m * 2 + 1), bl));
-                            }
-                        }
-                    }
-
-                    nativeImages[i] = nativeImage2;
-                }
-            }
-
-            return nativeImages;
-        }
+        return original;
     }
 }
